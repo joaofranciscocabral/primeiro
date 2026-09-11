e79490cad6b71089ddd31a92a8bf03d00def0950
Igor Dayan <165727764+SMIDE123@users.noreply.github.com>
2026-09-10 21:18:55 -0400

    Modificações feitas

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void exibir_menu(void) {
     printf("\n=== CONTROLE DE ESTOQUE ===\n");
     printf("1 - Listar produtos\n");
     printf("2 - Exibir valor total em estoque\n");
+    printf("4 - Exibir total a prazo (com juros)\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
 }
@@ -55,6 +56,10 @@ int main(void) {
             case 2:
                 printf("\nTotal em estoque: R$ %.2f\n", calcular_total(estoque, total_produtos));
                 break;
+            case 4:
+                printf("\nTotal a prazo: R$ %.2f\n",
+                    aplicar_juros(calcular_total(estoque, total_produtos)));
+                break;
             case 0:
                 printf("\nEncerrando o programa...\n");
                 break;
@@ -65,4 +70,8 @@ int main(void) {
     }
 
     return 0;
+}
+
+float aplicar_juros(float total) {
+    return total * (1 + TAXA_JUROS);
 }
